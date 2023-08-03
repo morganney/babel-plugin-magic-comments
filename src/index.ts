5beebb4f0fb0331102429fb07b5b376af3525aac
Morgan Ney <morganney@gmail.com>
2023-08-03 10:07:34 -0500

    chore: add types.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
 import { relative } from 'node:path'
 
+import { declare } from '@babel/helper-plugin-utils'
 import { schema, getMagicComment } from 'magic-comments'
 import { validate } from 'schema-utils'
 
-const hasNoComments = (node) => {
-  return ['leading', 'inner', 'trailing'].every((prefix) => !node[`${prefix}Comments`])
+import type { MagicComments, Match } from 'magic-comments'
+import type { Schema } from 'schema-utils/declarations/validate.js'
+import { Expression } from '@babel/types'
+
+interface Options extends MagicComments {
+  match?: Match
+  verbose?: boolean
 }
 
-export default function () {
+const hasNoComments = (node: Expression) => {
+  return !node.leadingComments && !node.trailingComments && !node.innerComments
+}
+
+export default declare((api, opts: Options) => {
+  api.assertVersion(7)
+
   return {
     visitor: {
-      CallExpression(path, state) {
-        const { node } = path
-        const { opts } = state
-
-        validate(schema, opts, {
+      CallExpression(path) {
+        validate(schema as Schema, opts, {
           name: 'babel-plugin-magic-comments',
           baseDataPath: 'options'
         })
@@ -29,9 +38,10 @@ export default function () {
            * @see https://github.com/tc39/proposal-import-attributes
            */
           const specifier = path.get('arguments')[0]
-          const specifierValue = code.slice(specifier.node.start, specifier.node.end)
+          const { start, end } = specifier.node
+          const specifierValue = code.slice(start as number, end as number)
 
-          if (hasNoComments(specifier.node)) {
+          if (hasNoComments(specifier.node as Expression) && filename) {
             const magicComment = getMagicComment({
               match,
               open: true,
@@ -44,9 +54,10 @@ export default function () {
               specifier.addComment('leading', magicComment)
 
               if (verbose) {
-                const relativeFilename = relative(root, filename)
+                const { node } = path
+                const relativeFilename = relative(root as string, filename)
                 const updatedImport = code
-                  .slice(node.start, node.end)
+                  .slice(node.start as number, node.end as number)
                   .replace(specifierValue, `/*${magicComment}*/ ${specifierValue}`)
 
                 // eslint-disable-next-line no-console
@@ -58,4 +69,6 @@ export default function () {
       }
     }
   }
-}
+})
+
+export type { Options }
